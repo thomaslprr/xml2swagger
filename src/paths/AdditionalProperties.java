d98975cab02ce59532258025103e72f8ddd86f4c
Thomas Lapierre <thomaslapierre@orange.fr>
2021-06-07 16:11:38 +0200

    improvement of swagger object and java class relation

---
+++ src/paths/AdditionalProperties.java
@@ -0,0 +1,15 @@
+package paths;
+
+public class AdditionalProperties {
+	
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
